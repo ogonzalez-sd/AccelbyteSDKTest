e2987883e7cb6ff6bd57560f4c639fbf228bf0c2
Omar Gonzalez <ogonzalez@secretdoorgames.com>
2022-04-12 17:34:36 -0700

    Initial commit

---
+++ Assets/Accelbyte.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AccelByte.Api;
+using AccelByte.Core;
+using UnityEngine;
+
+public class Accelbyte : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        User user = AccelBytePlugin.GetUser();
+        user?.LoginWithDeviceId(OnLoginResult);
+    }
+
+    private void OnLoginResult(Result result)
+    {
+        if (result.IsError)
+        {
+            Debug.LogError(result.Error.Message);
+        }
+        else
+        {
+            Debug.Log("success");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
